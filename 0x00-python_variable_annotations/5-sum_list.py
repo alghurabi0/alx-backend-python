a1f6e3a05cdb312be345a6e5889df8833f2dbeb2
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-14 17:19:48 +0000

    kjflsd

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """type-annotated func retuns sum of lost of floats."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """type-annotated func retuns sum of lost of floats."""
     x: float = 0.0
     for num in input_list:
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """type-annotated func returns sum of ints and floats in list."""
+from typing import List, Union
 
-
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """type-annotated func returns sum of ints and floats in list."""
     x: float 0.0
     for num in mxd_lst:
