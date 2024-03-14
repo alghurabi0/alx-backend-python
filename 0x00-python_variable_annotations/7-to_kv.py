6b605ac544ce7cc6f1fbfa78757945682c9aa516
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-14 17:31:26 +0000

    tupllle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Typle[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ type-annotated func """
     return (k, float(v**2))
