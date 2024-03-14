82fbf3208c2fdb4ef0cb9e0e34c964eb1b5cc6de
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-14 17:27:03 +0000

    fjskdjf

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """type-annotated func returns sum of ints and floats in list."""
-    x: float 0.0
+    x: float = 0.0
     for num in mxd_lst:
         x += num
     return x
