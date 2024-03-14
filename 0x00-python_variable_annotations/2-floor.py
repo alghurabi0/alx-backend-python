84bc4fccebe10238eea2d19ca08d3dd775dc297f
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-14 17:02:49 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" type annotated func that returns the floor of a float """
+import math
+
+
+def floor(n: float) -> int:
+    """ type annotated func that returns the floor of a float"""
+    return math.floor(n)
