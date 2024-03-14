e7114ca42aff03599bedea05cfdac1909d678f9b
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-14 17:04:46 +0000

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type annotated func returns str rep of float """
+
+
+def to_str(n: float) -> str:
+    """ type-annotated func return str rep of float."""
+    return str(n)
