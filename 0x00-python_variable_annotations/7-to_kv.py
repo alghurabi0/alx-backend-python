1462579a3f01a8ff2deda59b5c0c3c64b13533a1
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-14 17:25:27 +0000

    fjdks

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/in/env python3
+""" type-annotated func """
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Typle[str, float]:
+    """ type-annotated func """
+    return (k, float(v**2))
