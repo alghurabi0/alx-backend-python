0839e2eb7f1ebd1e731487f55a8d9e3be001d43f
alghurabi0 <zaid.alghurabi01@gmail.com>
2024-03-20 01:54:54 +0000

    kfjsfj

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,11 @@
 """ async python generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    """ async python generator """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
